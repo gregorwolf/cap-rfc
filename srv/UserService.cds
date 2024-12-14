549bd4858ee4623380cbcce346be3cb80de9771a
Gregor Wolf <gregor.wolf@gmail.com>
2024-12-14 21:47:10 +0000

    return user list

--- srv/UserService.cds
+++ srv/UserService.cds
@@ -1,7 +1,7 @@
-using {SAP_ABAP_BACKEND_RFC as external} from './external/SAP_ABAP_BACKEND_RFC';
+using {SAP_ABAP_BACKEND_RFC.DDIC.BAPIUSNAME as BAPIUSNAME} from './external/SAP_ABAP_BACKEND_RFC';
 
 service UserService {
 
-    function getUsers() returns array of external.BAPI_USER_GETLIST.ResultType;
+    function getUsers() returns array of BAPIUSNAME;
 
 }
--- srv/UserService.js
+++ srv/UserService.js
@@ -1,14 +1,12 @@
-const cds = require('@sap/cds')
+const cds = require("@sap/cds");
 class UserService extends cds.ApplicationService {
-   init() {
-      this.on('getUsers', async (req) => {
-
-         const sys = await cds.connect.to('SAP_ABAP_BACKEND_RFC')
-         const userData = await sys.BAPI_USER_GETLIST({ MAX_ROWS: 10 })
-         return userData
-
-      })
-      return super.init()
-   }
+  init() {
+    this.on("getUsers", async (req) => {
+      const sys = await cds.connect.to("SAP_ABAP_BACKEND_RFC");
+      const userData = await sys.BAPI_USER_GETLIST({ MAX_ROWS: 10 });
+      return userData.USERLIST;
+    });
+    return super.init();
+  }
 }
-module.exports = UserService
+module.exports = UserService;
