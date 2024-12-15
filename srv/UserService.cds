f7e8b5e2b41f01dbae66954eb89828bfda9ead1b
Gregor Wolf <gregor.wolf@gmail.com>
2024-12-15 09:32:23 +0000

    implement Users Entity

--- srv/UserService.cds
+++ srv/UserService.cds
@@ -1,6 +1,10 @@
 using {SAP_ABAP_BACKEND_RFC.DDIC.BAPIUSNAME as BAPIUSNAME} from './external/SAP_ABAP_BACKEND_RFC';
 
 service UserService {
+    @cds.persistence.skip
+    entity Users : BAPIUSNAME {
+
+    }
 
     function getUsers() returns array of BAPIUSNAME;
 
--- srv/UserService.js
+++ srv/UserService.js
@@ -1,8 +1,14 @@
 const cds = require("@sap/cds");
 class UserService extends cds.ApplicationService {
-  init() {
+  async init() {
+    const sys = await cds.connect.to("SAP_ABAP_BACKEND_RFC");
+
+    this.on('READ', 'Users', async req => {
+      const userData = await sys.BAPI_USER_GETLIST({ MAX_ROWS: 10 });
+      return userData.USERLIST;
+    });
+
     this.on("getUsers", async (req) => {
-      const sys = await cds.connect.to("SAP_ABAP_BACKEND_RFC");
       const userData = await sys.BAPI_USER_GETLIST({ MAX_ROWS: 10 });
       return userData.USERLIST;
     });
