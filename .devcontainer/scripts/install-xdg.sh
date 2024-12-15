adf74076b6849d4d2278aeff7e6aab122e56c2e4
Gregor Wolf <gregor.wolf@gmail.com>
2024-12-15 09:28:50 +0000

    install tools for BTP deployment

--- .devcontainer/scripts/install-xdg.sh
+++ .devcontainer/scripts/install-xdg.sh
@@ -1,5 +1,9 @@
 # Install xdg-utils to enable opening a browser from devcontainer
 # Currently no out-of-the box support available see: https://github.com/devcontainers/images/issues/885
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo gpg --dearmor -o /usr/share/keyrings/cli.cloudfoundry.org.gpg
+echo "deb [signed-by=/usr/share/keyrings/cli.cloudfoundry.org.gpg] https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get update
-sudo apt-get install -y xdg-utils
-npm i -g @sap/cds-dk
+sudo apt-get install -y xdg-utils cf8-cli
+cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org
+cf install-plugin multiapps -f
+npm i -g @sap/cds-dk mbt
